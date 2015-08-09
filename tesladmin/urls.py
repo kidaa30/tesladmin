81f509a0446b79978a4602eff4b85ea63a5f603d
Ryan Gonzalez <rymg19@gmail.com>
2015-08-09 13:46:02 -0500

    URL fixes

--- tesladmin/urls.py
+++ tesladmin/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import RedirectView
 from django.contrib import admin
 from . import settings
 import os
 
 urlpatterns = patterns('',
+    url(r'^$', RedirectView.as_view(url='site/index.html', permanent=False)),
     url(r'^site/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': 'static/output' if settings.DEBUG\
                                          else os.path.join(settings.STATIC_ROOT,
                                                            'output')
     }),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^tinymce/', include('tinymce.urls')),
 )
